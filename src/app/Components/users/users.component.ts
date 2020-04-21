1644beb73097d13746806056cd690f606eb564c8
JAvier <xavier.laurean@gmail.com>
2020-04-21 12:51:27 -0400

    loading

--- src/app/Components/users/users.component.ts
+++ src/app/Components/users/users.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import{ UsersService } from '../../Services/users.service';
-import { Subject } from 'rxjs';
+import { Subject,interval,timer } from 'rxjs';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html'
@@ -8,22 +8,39 @@ import { Subject } from 'rxjs';
 })
 export class UsersComponent implements OnInit {
 
+  public loading: boolean;
+
   dtOptions: DataTables.Settings = {};
   Usuarios:any=[];
 
   dtTrigger: Subject<any> = new Subject<any>();
 
-  constructor(public Users: UsersService) { }
+  constructor(public Users: UsersService) {
+
+      this.loading=true;
+
+   }
 
   ngOnInit(){
-    this.getUsuarios();
+        this.getUsuarios();
+        if(!this.getUsuarios){
+          alert('no se puede recuperar informacion de Rehive')
+        }else{
+          this.loading=false;
+        }
 
-    this.dtOptions = {
-      responsive: true,
-      pagingType: 'full_numbers',
-      pageLength: 15,
-     
-  }
+        this.dtOptions = {
+        responsive: true,
+        pagingType: 'full_numbers',
+        pageLength: 15, }
+
+        
+//Contador de segundos interval contador y timer para mostrar despues de cierto tiempo
+ //       const timerr= interval(4000);
+
+  //  timerr.subscribe((n)=>{console.log('Cantidad de tiempo',n)
+   // });
+
 }
   getUsuarios(){
     this.Usuarios=[];
@@ -34,7 +51,9 @@ export class UsersComponent implements OnInit {
       //que exista un cambio
       this.dtTrigger.next();
     });
+   
   }
+
 
 
 }
