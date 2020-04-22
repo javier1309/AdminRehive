483a38b7e82c830507b6627e788e8590660d4e3d
JAvier <xavier.laurean@gmail.com>
2020-04-22 11:11:14 -0400

    conferencia cristian

--- src/app/Services/users.service.ts
+++ src/app/Services/users.service.ts
@@ -3,7 +3,7 @@ import{HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
 import{Observable, of} from 'rxjs';
 
 
-const endpoint='https://api.rehive.com/3/admin/users/?page_size=600';
+const endpoint='https://api.rehive.com/3/admin/users/?page=2&page_size=25';
 const httpOptions = {  // contruyendo el http options
   headers: new HttpHeaders({
     'Content-Type':  'application/json',
@@ -14,7 +14,7 @@ const httpOptions = {  // contruyendo el http options
 }
 
  //dando atributos al header
- httpOptions.headers =
+
  httpOptions.headers.set('Authorization', 'Token 1b352780ddbb6ad9e415a843572ecb7202f2fcd1dbed7770f1d735b8987bbef6');
  
 
@@ -29,16 +29,22 @@ export class UsersService {
 
   constructor(private http: HttpClient) { }
 
-
+ 
 
 
   getUsers(): Observable<any> {
    
-    
-    return this.http.get(endpoint,httpOptions);   //get para retornar valores
-
-
+    let users=this.http.get(endpoint,httpOptions);
+    console.log(users);
+    return users ;  //get para retornar valores
   } 
+
+  //getUsers(page=1, size=20): Observable<any> {
+   // const endpoint= `https://api.rehive.com/3/admin/users/?page=${page}&page_size=${size}`;
+   // let users=this.http.get(endpoint,httpOptions);
+  //  console.log(users);
+  //  return users ;  //get para retornar valores
+ // } 
 
 
 
